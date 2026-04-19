bc2d5963c8617a40040065d76f48e53948687017
99amiqur <99amiqur@gafe.molndal.se>
2026-04-19 11:59:10 +0200

    Intial project setup

---
+++ Models/JobSeeker.cs
@@ -0,0 +1,10 @@
+namespace JobMatch.API.Models
+{
+    public class JobSeeker
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Skills { get; set; } = string.Empty;
+    }
+}
